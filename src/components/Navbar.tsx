bfa0c9102571632b1e85b30d3436ed32ee3c68de
ssulochanaherath <ssulochanaherath.com>
2025-02-17 19:06:43 +0530

    Few Changes.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -37,7 +37,7 @@ function Navbar() {
                 <div className="flex items-center space-x-2">
                     <FaFilm className="text-white text-2xl" />
                     <div className="text-white text-2xl font-bold">
-                        <Link to="/">Film Hall</Link>
+                        <Link to="/dashboard">Film Hall</Link>
                     </div>
                 </div>
 

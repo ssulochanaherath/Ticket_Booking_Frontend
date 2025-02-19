f1fccc784b2177cf98ed7d0fa736d1770ba397ed
ssulochanaherath <ssulochanaherath.com>
2025-02-19 21:58:35 +0530

    App modified.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';  // Import Provider
-import { store } from './store/Store';   // Import your configured Redux store
+import { Provider } from 'react-redux';
+import store from './store/Store'; // Ensure correct import
+
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
-import Customer from './pages/Customer.tsx';
-import Films from './pages/Films.tsx';
-import Seats from './pages/Seats.tsx';
+import Customer from './pages/Customer';
+import Films from './pages/Films';
+import Seats from './pages/Seats';
 
 function App() {
     return (
-        <Provider store={store}>  {/* Wrap your Router with Provider */}
+        <Provider store={store}>
             <Router>
                 <Routes>
                     <Route path="/" element={<Login />} />
                     <Route path="/dashboard" element={<Dashboard />} />
                     <Route path="/films" element={<Films />} />
                     <Route path="/customer" element={<Customer />} />
-                    <Route path="/login" element={<Login />} />
                     <Route path="/seats" element={<Seats />} />
                 </Routes>
             </Router>
